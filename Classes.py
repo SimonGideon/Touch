27e6e5b004ae4f3c1712917cbd75393dff33bd52
SimonGideon <simongideon918@gmail.com>
2021-05-21 16:43:07 +0300

    D2 classes

--- Classes.py
+++ Classes.py
@@ -29,9 +29,26 @@ class Person:
         self.firstname = fname
         self.lastname = lname
 
+# Child and parent class.
+    #Parent Class.
     def printname(self):
         print(self.firstname, self.lastname)
 
 
 x = Person("John", "Doe")
 x.printname()
+
+# Child class.
+class Student(Person):
+    pass
+# 'Pass' used when you do not add any other properties or method to the class.
+x = Student("Mike", "Sonko")
+x.printname()
+
+"""'__init__() is called automatically every time the class is being used to 
+create a new object"""
+class Student(Person):
+    def __init__(self, frame, lname):
+
+class Student(Person):
+    def __init__(self):

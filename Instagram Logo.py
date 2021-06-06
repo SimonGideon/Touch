33d2ceff641d6fde6ba178e5abdf155f611ac5bd
SimonGideon <simongideon918@gmail.com>
2021-06-06 15:03:17 +0300

    IG Logo

---
+++ Instagram Logo.py
@@ -0,0 +1,22 @@
+import turtle
+from turtle import*
+turtle.bgcolor("black")
+pencolor('yellow')
+width(23)
+penup()
+goto(160,-100)
+pendown()
+
+left(90)
+for i in range(4):
+    forward(250)
+    circle(34,90)
+
+penup()
+goto(80,30)
+pendown()
+circle(80,30)
+penup()
+goto(110,130)
+pendown()
+circle(7,360)

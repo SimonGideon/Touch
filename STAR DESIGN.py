bce2e824f5d7418f217251a658e8be6f67e19ddb
SimonGideon <simongideon918@gmail.com>
2021-06-09 00:11:55 +0300

    Hands on Turtle

---
+++ STAR DESIGN.py
@@ -0,0 +1,14 @@
+from turtle import *
+import turtle
+col = ('yellow', 'red', 'orange', 'blue', 'white')
+t = turtle.Turtle()
+screen = turtle.screen()
+screen.bgcolor('black')
+t.speen(30)
+
+
+for i in range (150):
+    i.color(col[i%6])
+    t.forward(i*4)
+    t.left(150)
+    t.width(2)
